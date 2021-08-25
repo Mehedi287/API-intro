f60908887c6c27cc1a8818255c2e5722754369f7
Mehedi390 <mh0816150@gmail.com>
2021-08-25 10:35:09 -0700

    objact to stringify and stringify to objace learnd

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-console.log('hello api');
-const h = 'him ama';
-console.log(h);
+const palyer = { name: 'hasan', id: 09, phone: 098090, ft: 'kuddus' };
+console.log(palyer);
+const singy = JSON.stringify(palyer);
+console.log(singy);
+const shop = {
+    name: 'motu',
+    product: 'shomosa',
+    bake: false,
+    anotherProudct: ['puri', 'singra', 'laddu']
+};
+const motiSing = JSON.stringify(shop);
+console.log(motiSing);
+const converToObjact = JSON.parse(motiSing);
+console.log(converToObjact);
